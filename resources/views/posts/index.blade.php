6ed7ba48d05320f945f2c5973ef5c462372d46b9
jintan-aka <tanakajin1@gmail.com>
2024-03-04 20:12:35 +0900

    記事の削除

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,8 +17,26 @@
                     
                     <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
                     <p class='body'>{{ $post->body }}</p>
+                 <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button" onclick="deletePost({{ $post->id }})">delete</button> 
+                 </form>
                 </div>
              @endforeach
         </div>
+        
+        <script>
+    function deletePost(id) {
+        'use strict'
+
+        if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+            document.getElementById(`form_${id}`).submit();
+        }
+    }
+    </script>
+    
+    `form_${id}`
+    
     </body>
 </html> 
